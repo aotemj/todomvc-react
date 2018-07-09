fb65d29a6186d2f3ac711e3c90f981da3d90495b
zxl <991807272@qq.com>
2018-07-09 23:53:46 +0800

    #新增底部静态布局#

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,29 +1,31 @@
-import React,{Component} from "react";
+import React, {Component} from "react";
 import TodoItem from "../TodoItem/TodoItem"
+
 class Main extends Component {
-    render(){
+    render() {
         return (
             <section className="main">
-				<input id="toggle-all" className="toggle-all" type="checkbox"/>
-				<label htmlFor="toggle-all">Mark all as complete</label>
-				<ul className="todo-list">
+                <input id="toggle-all" className="toggle-all" type="checkbox"/>
+                <label htmlFor="toggle-all">Mark all as complete</label>
+                <ul className="todo-list">
                     {
-                        this.props.todoList.map((item,index)=>{
+                        this.props.todoList.map((item, index) => {
                             return (
-                                <TodoItem 
-                                content={item.content} key={index} completed={item.completed} 
-                                editing = {item.editing}
-                                toggleStatus={this.props.toggleStatus}
-                                toggleEditing={this.props.toggleEditing}
-                                index={index}
-                                delItem={this.props.delItem}
+                                <TodoItem
+                                    content={item.content} key={index} completed={item.completed}
+                                    editing={item.editing}
+                                    toggleStatus={this.props.toggleStatus}
+                                    toggleEditing={this.props.toggleEditing}
+                                    index={index}
+                                    delItem={this.props.delItem}
                                 />
                             )
                         })
                     }
                 </ul>
-			</section>
+            </section>
         )
     }
 }
+
 export default Main
