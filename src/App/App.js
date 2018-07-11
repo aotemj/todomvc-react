15032397d2866b32cce320e20e0b02d40c12e6ef
zhaoxinlei <991807272@qq.com>
2018-07-11 21:55:43 +0800

    #修改文件格式#

--- src/App/App.js
+++ src/App/App.js
@@ -5,106 +5,112 @@ import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
 
 class App extends Component {
-    constructor() {
-        super();
-        this.addNewTodo = this.addNewTodo.bind(this)
-        this.toggleStatus = this.toggleStatus.bind(this)
-        this.delItem = this.delItem.bind(this)
-        this.toggleEditing = this.toggleEditing.bind(this)
-        this.state = {
-            todoList: [
-                {
-                    content: '123',
-                    completed: true,
-                    editing: false
-                },
-                {
-                    content: '45',
-                    completed: false,
-                    editing: true
-                }
-            ]
-        }
-    }
+  constructor() {
+    super();
+    this.addNewTodo = this.addNewTodo.bind(this);
+    this.toggleStatus = this.toggleStatus.bind(this);
+    this.delItem = this.delItem.bind(this);
+    this.toggleEditing = this.toggleEditing.bind(this);
+    this.state = {
+      todoList: [
+        {
+          content: '123',
+          completed: true,
+          editing: false,
+        },
+        {
+          content: '45',
+          completed: false,
+          editing: true,
+        },
+      ],
+    };
+  }
 
-    // 新增todo
-    addNewTodo(newTodo) {
-        let todoList = this.state.todoList;
-        todoList.push({
-            content: newTodo,
-            completed: false
-        });
-        this.setState({
-            todoList
-        })
-    }
+  // 新增todo
+  addNewTodo(newTodo) {
+    const { todoList } = this.state.todoList;
+    todoList.push({
+      content: newTodo,
+      completed: false,
+    });
+    this.setState({
+      todoList,
+    });
+  }
 
-    // 切换完成
-    toggleStatus(index) {
-        let todoList = this.state.todoList;
-        let completed = todoList[index].completed;
-        todoList[index].completed = !completed
-        this.setState({
-            todoList
-        })
-    }
+  // 切换完成
+  toggleStatus(index) {
+    let todoList = this.state.todoList;
+    let completed = todoList[index].completed;
+    todoList[index].completed = !completed;
+    this.setState({
+      todoList,
+    });
+  }
 
-    // 删除todo
-    delItem(index) {
-        let todoList = this.state.todoList;
-        todoList.splice(index, 1)
-        this.setState({
-            todoList
-        })
-    }
+  // 删除todo
+  delItem(index) {
+    let todoList = this.state.todoList;
+    todoList.splice(index, 1);
+    this.setState({
+      todoList,
+    });
+  }
 
-    // 切换编辑
-    toggleEditing(index) {
-        let todoList = this.state.todoList;
-        let editing = todoList[index].editing
-        todoList.forEach((item, index1) => {
-            if (index1 !== index) {
-                item.editing = false
-            }
-        })
-        todoList[index].editing = !editing;
-        this.setState({
-            todoList
-        })
-    }
+  // 切换编辑
+  toggleEditing(index) {
+    let todoList = this.state.todoList;
+    let editing = todoList[index].editing;
+    todoList.forEach((item, index1) => {
+      if (index1 !== index) {
+        item.editing = false;
+      }
+    });
+    todoList[index].editing = !editing;
+    this.setState({
+      todoList,
+    });
+  }
 
-    render() {
-        return (
-            <div className="App">
-                <section className="todoapp">
-                    <Header addNewTodo={this.addNewTodo}/>
-                    <Main
-                        todoList={this.state.todoList}
-                        toggleStatus={this.toggleStatus}
-                        toggleEditing={this.toggleEditing}
-                        delItem={this.delItem}
-                    />
-                    <footer id="footer" style={{display: 'block'}}>
-                        <span id="todo-count"><strong>1</strong> item left</span>
-                        <ul id="filters">
-                            <li>
-                                <a className="selected" href="#/all">All</a>
-                            </li>
-                            <li>
-                                <a href="#/active">Active</a>
-                            </li>
-                            <li>
-                                <a href="#/completed">Completed</a>
-                            </li>
-                        </ul>
+  render() {
+    return (
+      <div className="App">
+        <section className="todoapp">
+          <Header addNewTodo={this.addNewTodo} />
+          <Main
+            todoList={this.state.todoList}
+            toggleStatus={this.toggleStatus}
+            toggleEditing={this.toggleEditing}
+            delItem={this.delItem}
+          />
+          <footer id="footer" style={{ display: 'block' }}>
+            <span id="todo-count">
+              <strong>
+														1
+</strong>
+item left
+            </span>
+            <ul id="filters">
+              <li>
+                <a className="selected" href="#/all">
+                  All
+                </a>
+              </li>
+              <li>
+                <a href="#/active">Active</a>
+              </li>
+              <li>
+                <a href="#/completed">Completed</a>
+              </li>
+            </ul>
+          </footer>
+        </section>
 
-                    </footer>
-                </section>
-
-                <Footer/>
-            </div>
-        );
-    }
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
